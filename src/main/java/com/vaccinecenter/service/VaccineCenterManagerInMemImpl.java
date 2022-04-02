b150ea241a53c0ca421ebb4d83c775ff6197f291
Harshit Panwar <harshit.panwar@nextuple.com>
2022-04-02 20:08:02 +0530

    updated availablity methods

--- src/main/java/com/vaccinecenter/service/VaccineCenterManagerInMemImpl.java
+++ src/main/java/com/vaccinecenter/service/VaccineCenterManagerInMemImpl.java
@@ -14,35 +14,30 @@ public class VaccineCenterManagerInMemImpl implements VaccineCenterManager {
 
     @Override
     public boolean add(VaccineCenter vaccineCenter) {
-        int len = vaccineCenterMap.size();
-        vaccineCenterMap.put(vaccineCenter.getId(),vaccineCenter);
         if(vaccineCenter.getId() == null)
             return false;
+        else if(get(vaccineCenter.getId()) != null)
+            return false;//if vaccine center already exist
 
-
-        if(len+1==vaccineCenterMap.size())
-            return true;
-        else
-            return false;
-
+        vaccineCenterMap.put(vaccineCenter.getId(),vaccineCenter);
+        return true;
     }
 
     @Override
     public boolean addAvailability(String vaccineCenterId, VaccineAvailability vaccineAvailability) {
-        VaccineCenter vaccineCenter = vaccineCenterMap.containsKey(vaccineCenterId)?
-                vaccineCenterMap.get(vaccineCenterId):null;
+        VaccineCenter vaccineCenter = get(vaccineCenterId);
         if(Objects.isNull(vaccineCenter)) return false;
 
         List<VaccineAvailability> vaccineAvailabilities = vaccineCenter.getVaccineAvailabilities();
+
         if(vaccineAvailabilities.isEmpty()){
             List<VaccineAvailability> vaccineAvailabilities1 = new ArrayList<>();
             vaccineAvailabilities1.add(vaccineAvailability);
             vaccineCenter.setVaccineAvailabilities(vaccineAvailabilities1);
         }else{
             for(VaccineAvailability vaccineAvailability1 : vaccineAvailabilities){
-                if(vaccineAvailability1.getDoseType().equals(vaccineAvailability.getDoseType())&&
-                        vaccineAvailability1.getVaccineType().equals(vaccineAvailability.getVaccineType())){
-                    updateAvailability(vaccineCenterId,vaccineAvailability);
+                if(vaccineAvailability1.equals(vaccineAvailability)){
+                    return false;
                 }
             }
             vaccineAvailabilities.add(vaccineAvailability);
@@ -53,36 +48,32 @@ public class VaccineCenterManagerInMemImpl implements VaccineCenterManager {
 
     @Override
     public boolean updateAvailability(String vaccineCenterId, VaccineAvailability vaccineAvailability) {
-        VaccineCenter vaccineCenter = vaccineCenterMap.containsKey(vaccineCenterId)?
-                vaccineCenterMap.get(vaccineCenterId):null;
+        VaccineCenter vaccineCenter = get(vaccineCenterId);
         if(Objects.isNull(vaccineCenter)) return false;
 
         List<VaccineAvailability> vaccineAvailabilities = vaccineCenter.getVaccineAvailabilities();
+        if(vaccineAvailabilities.isEmpty()) return false;
+
         int index = 0;
-        if(vaccineAvailabilities.isEmpty()){
-            return false;
-        }else{
-            for(VaccineAvailability vaccineAvailability1 : vaccineAvailabilities){
-                if(vaccineAvailability1.equals(vaccineAvailability)){
-                    vaccineAvailabilities.set(index,vaccineAvailability);
-                    return true;
-                }
-                index++;
+        for(VaccineAvailability vaccineAvailability1 : vaccineAvailabilities){
+            if(vaccineAvailability1.equals(vaccineAvailability)){
+                vaccineAvailabilities.set(index,vaccineAvailability);
+                return true;
             }
+            index++;
         }
         return false;
     }
 
     @Override
     public boolean removeAvailability(String vaccineCenterId, VaccineAvailability vaccineAvailability) {
-        VaccineCenter vaccineCenter = vaccineCenterMap.containsKey(vaccineCenterId)?
-                vaccineCenterMap.get(vaccineCenterId):null;
+        VaccineCenter vaccineCenter = get(vaccineCenterId);
         if(Objects.isNull(vaccineCenter)) return false;
         List<VaccineAvailability> vaccineAvailabilities = vaccineCenter.getVaccineAvailabilities();
+
+        if(vaccineAvailabilities.isEmpty()) return false;
+
         int index = 0;
-        if(vaccineAvailabilities.isEmpty()){
-            return false;
-        }
         for(VaccineAvailability vaccineAvailability1 : vaccineAvailabilities){
             if(vaccineAvailability1.equals(vaccineAvailability)){
                 vaccineAvailabilities.remove(index);
@@ -97,21 +88,21 @@ public class VaccineCenterManagerInMemImpl implements VaccineCenterManager {
 
     @Override
     public boolean bookVaccineSlot(String vaccineCenterId, VaccineType vaccineType, DoseType doseType) {
-        for(Map.Entry<String,VaccineCenter> vaccineCenterEntry : vaccineCenterMap.entrySet() ){
-            if(vaccineCenterEntry.getKey().equals(vaccineCenterId)){
-                List<VaccineAvailability> vaccineAvailabilityList = vaccineCenterEntry.getValue().getVaccineAvailabilities();
-                for(VaccineAvailability vaccineAvailability:vaccineAvailabilityList){
-                    if(vaccineAvailability.getVaccineType().equals(vaccineType)&&
+        VaccineCenter vaccineCenter = get(vaccineCenterId);
+        if(Objects.isNull(vaccineCenter)) return false;
+
+        List<VaccineAvailability> vaccineAvailabilities = vaccineCenter.getVaccineAvailabilities();
+
+        if(vaccineAvailabilities.isEmpty()) return false;
+
+        for(VaccineAvailability vaccineAvailability:vaccineAvailabilities){
+            if(vaccineAvailability.getVaccineType().equals(vaccineType)&&
                     vaccineAvailability.getDoseType().equals(doseType)&&
                     vaccineAvailability.getAvailableQuantityCount()>0){
-                        vaccineAvailability.setAvailableQuantityCount(
-                                vaccineAvailability.getAvailableQuantityCount()-1
-                        );;
-                        updateAvailability(vaccineCenterId,vaccineAvailability);
-                        return true;
-                    }
-
-                }
+                vaccineAvailability.setAvailableQuantityCount(
+                        vaccineAvailability.getAvailableQuantityCount()-1);
+                updateAvailability(vaccineCenterId,vaccineAvailability);
+                return true;
             }
         }
         return false;
@@ -120,16 +111,13 @@ public class VaccineCenterManagerInMemImpl implements VaccineCenterManager {
     @Override
     public SearchResponse search(VaccineType vaccineType, DoseType doseType) {
         int totalCount = 0;
-        SearchResponse searchResponse = new SearchResponse(0, Collections.emptyList());
+        SearchResponse searchResponse = new SearchResponse(0, new ArrayList<VaccineCenter>());
         for(Map.Entry<String,VaccineCenter> vaccineCenterEntry : vaccineCenterMap.entrySet() ){
-            List<VaccineAvailability> vaccineAvailabilityList = vaccineCenterEntry.getValue().getVaccineAvailabilities();
-            for(VaccineAvailability vaccineAvailability:vaccineAvailabilityList){
-                if(vaccineAvailability.getVaccineType().equals(vaccineType)&&
-                        vaccineAvailability.getDoseType().equals(doseType)&&
-                        vaccineAvailability.getAvailableQuantityCount()>0){
-                    totalCount++;
-                    searchResponse.getResults().add(vaccineCenterEntry.getValue());
-            }
+            VaccineCenter vaccineCenter = vaccineCenterEntry.getValue();
+            if(vaccineCenter.getVaccineTypes().contains(vaccineType) &&
+            vaccineCenter.getDoseTypes().contains(doseType)){
+                totalCount++;
+                searchResponse.getResults().add(vaccineCenter);
             }
         }
         searchResponse.setTotalCount(totalCount);
@@ -138,32 +126,19 @@ public class VaccineCenterManagerInMemImpl implements VaccineCenterManager {
 
     @Override
     public SearchResponse search(List<SearchRequest> searchRequestList) {
-        int totalCount = 0;
-        SearchResponse searchResponse = new SearchResponse(0, Collections.emptyList());
-        SearchResponse tempSearchResponse = new SearchResponse(0, Collections.emptyList());
+        SearchResponse searchResponse = new SearchResponse(0, new ArrayList<VaccineCenter>());
+        Set<VaccineCenter> vaccineCenterSet = new HashSet<>();
 
         for(SearchRequest searchRequest:searchRequestList){
-             tempSearchResponse = search(searchRequest.getVaccineType(),searchRequest.getDoseType());
+            SearchResponse tempSearchResponse = search(searchRequest.getVaccineType(),searchRequest.getDoseType());
+            vaccineCenterSet.addAll(tempSearchResponse.getResults());
         }
-        totalCount = searchResponse.getTotalCount() + tempSearchResponse.getTotalCount();
-        List<VaccineCenter> vaccineCenters = new ArrayList<>();
-        vaccineCenters.addAll(searchResponse.getResults());
-        vaccineCenters.addAll(tempSearchResponse.getResults());
-        searchResponse.setTotalCount(totalCount);
-        searchResponse.setResults(vaccineCenters);
+        searchResponse.setResults(new ArrayList<>(vaccineCenterSet));
+        searchResponse.setTotalCount(vaccineCenterSet.size());
         return searchResponse;
     }
 
     public VaccineCenter get(final String vaccineCenterId) {
         return vaccineCenterMap.get(vaccineCenterId);
-    }
-
-    public HashSet availableVaccineTypeAndDoseTypeAtCentre(List<VaccineAvailability> vaccineAvailabilityList){
-        HashSet<String> availableVaccineTypeAndDoseType = new HashSet<>();
-        for(VaccineAvailability vaccineAvailability:vaccineAvailabilityList){
-            availableVaccineTypeAndDoseType.add(vaccineAvailability.getVaccineType()+"-"+vaccineAvailability.getDoseType());
-        }
-        return availableVaccineTypeAndDoseType;
-
     }
 }
